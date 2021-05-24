a329b131fa8a16f40dc3f2cc9a2d548fa013d048
Danny Herpol <danny@lachjekrom.com>
2021-05-24 16:31:38 +0200

    Fix script session

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Author Danny
-# Version GIT: 2021-05-24 15:29
+# Version GIT: 2021-05-24 16:31
 
 # docker-entrypoint.sh 
 # script who is launched at startup of the container
@@ -15,16 +15,16 @@ CNORMAL="\e[0m"
 echo -e "== ${CGREEN}Start entrypoint ${0}${CNORMAL} =="
 
 # Setting proxy server
-set-proxy.sh
+. set-proxy.sh
 
 # Adding ca-certificates
-set-cacertificates.sh
+. set-cacertificates.sh
 
-# Adding SSH keys
-set-sshkeys.sh
+# Adding SSH keys in current session
+. set-sshkeys.sh
 
 # Echo buildversion
-show-imagedetails.sh
+. show-imagedetails.sh
 
 # End
 echo -e "== ${CGREEN}End entrypoint ${0}${CNORMAL} =="
